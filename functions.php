97c5524841e1af8d38167d410f6dce68afa68270
Alexander Barton <alexander.barton@outlook.com>
2016-06-21 22:20:44 +0200

    Moved the theme style to the css directory

--- functions.php
+++ functions.php
@@ -1,16 +1,26 @@
 <?php
 
-// Font Awesome support
-function theme_add_font_awesome() {
+// Default Style Support
+function ap_add_theme_style() {
+    wp_enqueue_style('theme-style-css', get_template_directory_uri() . '/css/style.css', array(
+        'font-awesome-css',
+        'bootstrap-css'
+    ));
+}
+
+add_action('wp_enqueue_scripts', 'ap_add_theme_style');
+
+// Font Awesome Support
+function ap_add_font_awesome() {
     wp_enqueue_style('font-awesome-css', get_template_directory_uri() . '/css/font-awesome.min.css');
 }
 
-add_action('wp_enqueue_scripts', 'theme_add_font_awesome');
+add_action('wp_enqueue_scripts', 'ap_add_font_awesome');
 
-// Bootstrap support
-function theme_add_bootstrap() {
+// Bootstrap Support
+function ap_add_bootstrap() {
     wp_enqueue_style('bootstrap-css', get_template_directory_uri() . '/css/bootstrap.min.css');
     wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '3.0.0', true);
 }
 
-add_action('wp_enqueue_scripts', 'theme_add_bootstrap');
+add_action('wp_enqueue_scripts', 'ap_add_bootstrap');
--- index.php
+++ index.php
@@ -5,7 +5,6 @@
 		<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
 		<div class="entry">
 			<?php the_content(); ?>
-			<i class="fa fa-camera-retro fa-lg"></i>
 		</div>
 	<?php endwhile; endif; ?>
 </div>
