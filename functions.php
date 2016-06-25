8d596eafe523faac3c4013c1c9b3c41b3e17c763
Alexander Barton <alexander.barton@outlook.com>
2016-06-25 10:02:15 +0200

    Optimized the header

--- functions.php
+++ functions.php
@@ -1,30 +1,32 @@
 <?php
 
 // Default Style Support
+add_action('wp_enqueue_scripts', 'ap_add_theme_style');
 function ap_add_theme_style() {
     wp_enqueue_style('theme-style-css', get_template_directory_uri() . '/css/style.css', array('font-awesome-css', 'bootstrap-css'));
 }
 
-add_action('wp_enqueue_scripts', 'ap_add_theme_style');
-
 // Default Script Support
+add_action('wp_enqueue_scripts', 'ap_add_theme_script');
 function ap_add_theme_script() {
     wp_enqueue_script('theme-script-js', get_template_directory_uri() . '/js/script.js', array('jquery'));
 }
 
-add_action('wp_enqueue_scripts', 'ap_add_theme_script');
-
 // Font Awesome Support
+add_action('wp_enqueue_scripts', 'ap_add_font_awesome');
 function ap_add_font_awesome() {
     wp_enqueue_style('font-awesome-css', get_template_directory_uri() . '/css/font-awesome.min.css');
 }
 
-add_action('wp_enqueue_scripts', 'ap_add_font_awesome');
-
 // Bootstrap Support
+add_action('wp_enqueue_scripts', 'ap_add_bootstrap');
 function ap_add_bootstrap() {
     wp_enqueue_style('bootstrap-css', get_template_directory_uri() . '/css/bootstrap.min.css');
     wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '3.0.0', true);
 }
 
-add_action('wp_enqueue_scripts', 'ap_add_bootstrap');
+// Title Tag Support
+add_action('after_setup_theme', 'ap_add_title_tag');
+function ap_add_title_tag() {
+    add_theme_support('title-tag');
+}
--- header.php
+++ header.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
-    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+    <meta charset="<?php bloginfo('charset') ?>" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+    <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS2 Feed" href="<?php bloginfo('rss2_url'); ?>" />
 
-    <title><?php wp_title(); ?></title>
+    <!--[if IE]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
