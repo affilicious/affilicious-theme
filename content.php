83c4711bb2072a6c935d2693ebc349193b4bb073
Alexander Barton <alexander.barton@outlook.com>
2016-07-15 00:27:13 +0200

    Optimized the front page layout

--- content.php
+++ content.php
@@ -2,7 +2,7 @@
     <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
         <div class="post-body">
             <header class="post-header">
-                <?php if(has_category()): ?>
+                <?php if(!is_front_page() && has_category()): ?>
                     <span class="post-category"><?php the_category(', '); ?></span>
                 <?php endif; ?>
                 <h1 class="post-title"><?php the_title(); ?></h1>
@@ -17,22 +17,24 @@
                 <?php the_content(); ?>
             </div>
         </div>
-        <footer class="post-footer">
-            <!--<ul class="post-meta">
-                    <li>
-                        <time><?php the_date('d.m.Y'); ?></time>
-                    </li>
-                    <li>
-                        <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author">
-                            <?php echo get_the_author_meta('display_name'); ?>
-                        </a>
-                    </li>
-                    <li>
+        <?php if(!is_front_page()): ?>
+            <footer class="post-footer">
+                <!--<ul class="post-meta">
+                        <li>
+                            <time><?php the_date('d.m.Y'); ?></time>
+                        </li>
+                        <li>
+                            <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author">
+                                <?php echo get_the_author_meta('display_name'); ?>
+                            </a>
+                        </li>
+                        <li>
 
-                    </li>
-                </ul>-->
-            
-            <?php the_post_navigation(); ?>
-        </footer>
+                        </li>
+                    </ul>-->
+
+                <?php the_post_navigation(); ?>
+            </footer>
+        <?php endif; ?>
     </article>
 <?php endwhile; endif; ?>
