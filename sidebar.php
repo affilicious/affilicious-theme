5e40a206bd0b7f9d138a680d9afe928070481100
Alexander Barton <alexander.barton@outlook.com>
2016-07-22 13:15:43 +0200

    Fixed the sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,8 +1,8 @@
 <div class="col-md-4 col-xs-12">
-    <?php if(is_active_sidebar('sidebar-1')): ?>
+    <?php if(is_active_sidebar('main-sidebar')): ?>
         <aside class="sidebar">
             <ul>
-                <?php dynamic_sidebar('sidebar-1'); ?>
+                <?php dynamic_sidebar('main-sidebar'); ?>
             </ul>
         </aside>
     <?php endif; ?>
