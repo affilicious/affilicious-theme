8dae4a0784faa99fc2acff75bcaad1385d2546fa
Alexander Barton <alexander.barton@outlook.com>
2016-07-27 19:40:35 +0200

    Number fields are sanitized now for security purpose

--- src/Product/Detail/DetailGroupSetup.php
+++ src/Product/Detail/DetailGroupSetup.php
@@ -1,16 +1,24 @@
 <?php
 namespace ProjektAffiliateTheme\Product\Detail;
 
+use ProjektAffiliateTheme\Auxiliary\PostHelper;
+use ProjektAffiliateTheme\Product\Field\Field;
 use ProjektAffiliateTheme\Product\Field\FieldGroup;
 use ProjektAffiliateTheme\Product\Field\FieldGroupFactory;
 use Carbon_Fields\Container as CarbonContainer;
 use Carbon_Fields\Field as CarbonField;
 use ProjektAffiliateTheme\Product\Product;
+use ProjektAffiliateTheme\Product\ProductFactory;
 
 if(!defined('ABSPATH')) exit('Not allowed to access pages directly.');
 
 class DetailGroupSetup
 {
+    /**
+     * @var ProductFactory
+     */
+    private $productFactory;
+
     /**
      * @var FieldGroupFactory
      */
@@ -26,10 +34,13 @@ class DetailGroupSetup
      */
     public function __construct()
     {
+        $this->productFactory = new ProductFactory();
         $this->fieldGroupFactory = new FieldGroupFactory();
         $this->detailGroupFactory = new DetailGroupFactory();
 
         add_action('init', array($this, 'render'), 4);
+        add_action('added_post_meta', array($this, 'sanitize'), 10, 4);
+        add_action('updated_post_meta', array($this, 'sanitize'), 10, 4);
     }
 
     /**
@@ -67,7 +78,10 @@ class DetailGroupSetup
                         $field->getKey()
                     );
 
-                    $detail = CarbonField::make($field->getType(), $fieldId, $field->getLabel());
+                    // Carbon doesn't contain a number field. That's why we have to convert into a text field
+                    $type = $field->isNumber() ? Field::TYPE_TEXT : $field->getType();
+
+                    $detail = CarbonField::make($type, $fieldId, $field->getLabel());
 
                     if ($field->hasDefaultValue()) {
                         $detail->default_value($field->getDefaultValue());
@@ -86,6 +100,67 @@ class DetailGroupSetup
                 ->show_on_taxonomy_term($fieldGroup->getCategory(), Product::TAXONOMY)
                 ->set_priority('default')
                 ->add_fields($details);
+        }
+    }
+
+    /**
+     * @param int $metaId
+     * @param int $postId
+     * @param string $metaKey
+     * @param string $metaValue
+     */
+    public function sanitize($metaId, $postId, $metaKey, $metaValue)
+    {
+        if(strpos($metaKey, '_at_product_details') === 0) {
+            $post = PostHelper::getPost($postId);
+            $product = $this->productFactory->create($post);
+
+            $pattern = "/_at_product_details_(.+)_(.+)$/";
+            if(preg_match($pattern, $metaKey, $matches)) {
+                $detailGroupId = intval($matches[1]);
+                $key = $matches[2];
+
+                $detailType = null;
+                $detailGroups = $product->getDetailGroups();
+                foreach ($detailGroups as $detailGroup) {
+                    if ($detailGroup->getId() === $detailGroupId) {
+                        $details = $detailGroup->getDetails();
+                        foreach ($details as $detail) {
+                            if ($detail->getKey() === $key) {
+                                $detailType = $detail->getType();
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                if ($detailType === null) {
+                    return;
+                }
+
+                // Remove whitespace from the beginning and end
+                $metaValue = trim($metaValue);
+
+                switch ($detailType) {
+                    case Detail::TYPE_TEXT:
+                        $metaValue = filter_var($metaValue, FILTER_SANITIZE_STRING);
+                        break;
+                    case Detail::TYPE_NUMBER:
+                        $metaValue = str_replace(',', '.', $metaValue);
+                        $metaValue = preg_replace("/[^0-9+-,.]/","", $metaValue);
+                        $metaValue = floatval($metaValue);
+
+                        break;
+                    case Detail::TYPE_FILE:
+                        $metaValue = filter_var($metaValue, FILTER_SANITIZE_NUMBER_INT);
+                        break;
+                    default:
+                        break;
+                }
+
+                // Update the sanitized value
+                update_post_meta($postId, $metaKey, $metaValue);
+            }
         }
     }
 }
