b0c3951b3d25868ce66b35f88bbc6def27e3776b
Alexander Barton <alexander.barton@outlook.com>
2016-08-24 19:10:36 +0200

    Fixed some product image gallery styling bugs

--- assets/ts/script.ts
+++ assets/ts/script.ts
@@ -6,36 +6,33 @@ jQuery(function($) {
         $("[rel='tooltip']").tooltip();
     });
 
-    $(document).ready(function() {
-        $('.thumb-slider .slick-slider').slick({
-            slidesToShow: 4,
-            slidesToScroll: 1,
-            dots: false,
-            arrows: false,
-            focusOnSelect: true,
-            adaptiveHeight: true,
-            asNavFor: '.portfolio-slider .slick-slider'
-        });
 
-        $('.portfolio-slider .slick-slider').slick({
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            arrows: false,
-            asNavFor: '.thumb-slider .slick-slider',
-        });
-
-
-
-        var container = $('.portfolio-slider');
-        var content = $('.portfolio-slider .slick-list');
-        var containerHeight = container.height();
-        var contentHeight = content.height();
-
-        console.log(containerHeight);
-        console.log(contentHeight);
-
-        if (containerHeight > contentHeight) {
-            content.css('margin-top', (containerHeight - contentHeight) / 2);
-        }
+    $('.thumb-slider .slick-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: false,
+        focusOnSelect: true,
+        adaptiveHeight: true,
+        asNavFor: '.portfolio-slider .slick-slider'
     });
+
+    $('.portfolio-slider .slick-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: '.thumb-slider .slick-slider',
+    });
+
+    var container = $('.portfolio-slider');
+    var content = $('.portfolio-slider .slick-list');
+    var containerHeight = container.height();
+    var contentHeight = content.height();
+
+    console.log(containerHeight);
+    console.log(contentHeight);
+
+    if (containerHeight > contentHeight) {
+        content.css('margin-top', (containerHeight - contentHeight) / 2);
+    }
 });
