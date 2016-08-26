ce47f98b2d20c24408cb3c1420284675074325bb
Alexander Barton <alexander.barton@outlook.com>
2016-08-26 18:59:22 +0200

    Added an excerpt and a star rating

--- partials/content-product.php
+++ partials/content-product.php
@@ -2,68 +2,103 @@
 
 <?php $product = affilicious_get_product(); ?>
 <article id="product-<?php the_ID(); ?>" <?php post_class('product'); ?>>
-    <header class="product-header box">
-        <h1 class="product-title"><?php the_title(); ?></h1>
 
-        <div class="row">
-            <div class="col-md-5">
-                <?php if($imageGallery = affilicious_get_product_image_gallery($product)): ?>
-                    <div class="product-image-gallery">
-                        <div class="portfolio-slider">
-                            <div class="slick-slider">
-                                <?php foreach ($product->getImageGallery() as $image): ?>
-                                    <div class="slick-slider-item">
-                                        <?php echo wp_get_attachment_image($image, array(250, 250)); ?>
-                                    </div>
-                                <?php endforeach; ?>
-                            </div>
-                        </div>
-                        <div class="thumb-slider">
-                            <div class="slick-slider">
-                                <?php foreach ($product->getImageGallery() as $image): ?>
-                                    <div class="slick-slider-item">
-                                        <?php echo wp_get_attachment_image($image, array(50, 50)); ?>
-                                    </div>
-                                <?php endforeach; ?>
-                            </div>
-                        </div>
-                    </div>
-                <?php else: ?>
-                    <div class="product-thumbnail">
-                        <?php if(has_post_thumbnail()): ?>
-                            <?php the_post_thumbnail(); ?>
-                        <?php else: ?>
-                            <i class="fa fa-question-circle-o fa-2x"></i>
-                        <?php endif; ?>
-                    </div>
-                <?php endif; ?>
+    <header class="product-header">
+        <div class="panel">
+            <div class="panel-heading">
+                <h1 class="product-title"><?php the_title(); ?></h1>
+                <p class="product-rating">
+                    <i class="fa fa-star fa-lg" aria-hidden="true"></i>
+                    <i class="fa fa-star fa-lg" aria-hidden="true"></i>
+                    <i class="fa fa-star fa-lg" aria-hidden="true"></i>
+                    <i class="fa fa-star fa-lg" aria-hidden="true"></i>
+                    <i class="fa fa-star-half-o fa-lg" aria-hidden="true"></i>
+                    <span><?php echo sprintf(_n('based on %s review', 'based on %s reviews', 5, 'affilicious-theme'), 5); ?></span>
+                </p>
             </div>
-
-            <div class="col-md-7">
-                <?php $fields = affilicious_get_product_details(); ?>
-                <?php if(!empty($fields)): ?>
-                    <table class="product-table table table-striped">
-                        <tbody>
-                            <tr>
-                                <td><?php _e('Price', 'affilicious-theme'); ?></td>
-                                <td><?php echo affilicious_get_product_price($product); ?></td>
-                            </tr>
-                        <?php foreach ($fields as $field): ?>
-                            <tr>
-                                <td><?php echo $field['name']; ?></td>
-                                <?php if($field['type'] === 'file'): ?>
-                                    <td><?php echo wp_get_attachment_link($field['value'], 'medium', false, false, __('Download', 'affiliate-theme')); ?></td>
+            <div class="panel-body">
+                <div class="row">
+                    <div class="col-md-5">
+                        <div class="row">
+                            <div class="col-xs-12">
+                                <?php if($imageGallery = affilicious_get_product_image_gallery($product)): ?>
+                            <div class="product-image-gallery">
+                                <div class="portfolio-slider">
+                                    <div class="slick-slider">
+                                        <?php foreach ($product->getImageGallery() as $image): ?>
+                                            <div class="slick-slider-item">
+                                                <?php echo wp_get_attachment_image($image, array(250, 250)); ?>
+                                            </div>
+                                        <?php endforeach; ?>
+                                    </div>
+                                </div>
+                                <div class="thumb-slider">
+                                    <div class="slick-slider">
+                                        <?php foreach ($product->getImageGallery() as $image): ?>
+                                            <div class="slick-slider-item">
+                                                <?php echo wp_get_attachment_image($image, array(50, 50)); ?>
+                                            </div>
+                                        <?php endforeach; ?>
+                                    </div>
+                                </div>
+                            </div>
+                        <?php else: ?>
+                            <div class="product-thumbnail">
+                                <?php if(has_post_thumbnail()): ?>
+                                    <?php the_post_thumbnail(); ?>
                                 <?php else: ?>
-                                    <td><?php echo esc_html($field['value']); ?></td>
+                                    <i class="fa fa-question-circle-o fa-2x"></i>
                                 <?php endif; ?>
-                            </tr>
-                        <?php endforeach; ?>
-                        </tbody>
-                    </table>
-                <?php endif; ?>
+                            </div>
+                        <?php endif; ?>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-7">
+                        <?php if(has_excerpt()): ?>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="product-excerpt">
+                                        <?php the_excerpt(); ?>
+                                    </div>
+                                </div>
+                            </div>
+                        <?php endif; ?>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <?php $fields = affilicious_get_product_details(); ?>
+                                <?php if(!empty($fields)): ?>
+                                    <table class="product-details table table-striped">
+                                        <tbody>
+                                        <tr>
+                                            <td><?php _e('Price', 'affilicious-theme'); ?></td>
+                                            <td>
+                                                <a class="price" href="<?php echo affilicious_get_product_affiliate_link($product); ?>">
+                                                    <?php echo affilicious_get_product_price($product); ?>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                        <?php foreach ($fields as $field): ?>
+                                            <tr>
+                                                <td><?php echo $field['name']; ?></td>
+                                                <?php if($field['type'] === 'file'): ?>
+                                                    <td><?php echo wp_get_attachment_link($field['value'], 'medium', false, false, __('Download', 'affiliate-theme')); ?></td>
+                                                <?php else: ?>
+                                                    <td><?php echo esc_html($field['value']); ?></td>
+                                                <?php endif; ?>
+                                            </tr>
+                                        <?php endforeach; ?>
+                                        </tbody>
+                                    </table>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </header>
+
 
     <div class="product-body">
         <div class="panel">
@@ -78,7 +113,7 @@
                         <?php comments_number(
                             __('No comments', 'affiliate-theme'),
                             __('One comment', 'affiliate-theme'),
-                            __('% comments', 'affiliate-theme')
+                            __('%s comments', 'affiliate-theme')
                         ); ?>
                     </a>
                 </li>
