18b4f75331dc416e7d211ba48c6ccb9e208e88e2
Alexander Barton <alexander.barton@outlook.com>
2016-09-06 15:59:59 +0200

    Fixed the annoying link in the product image gallery

--- partials/content-product-image-gallery.php
+++ partials/content-product-image-gallery.php
@@ -1,9 +1,9 @@
-<?php if($imageGallery = affilicious_get_product_image_gallery($product)): ?>
-    <?php $affiliateLink = affilicious_get_product_affiliate_link($product); ?>
+<?php $affiliateLink = affilicious_get_product_affiliate_link($product); ?>
 
+<?php if($imageGallery = affilicious_get_product_image_gallery($product)): ?>
     <div class="product-image-gallery">
-        <a href="<?php echo $affiliateLink; ?>" rel="nofollow" target="_blank">
-            <div class="portfolio-slider">
+        <div class="portfolio-slider">
+            <a href="<?php echo $affiliateLink; ?>" rel="nofollow" target="_blank">
                 <div class="slick-slider">
                     <?php foreach ($imageGallery as $image): ?>
                         <div class="slick-slider-item" itemprop="image">
@@ -11,28 +11,26 @@
                         </div>
                     <?php endforeach; ?>
                 </div>
+            </a>
+        </div>
+        <div class="thumb-slider">
+            <div class="slick-slider">
+                <?php foreach ($imageGallery as $image): ?>
+                    <div class="slick-slider-item">
+                        <?php echo wp_get_attachment_image($image, array(50, 50)); ?>
+                    </div>
+                <?php endforeach; ?>
             </div>
-            <div class="thumb-slider">
-                <div class="slick-slider">
-                    <?php foreach ($imageGallery as $image): ?>
-                        <div class="slick-slider-item">
-                            <?php echo wp_get_attachment_image($image, array(50, 50)); ?>
-                        </div>
-                    <?php endforeach; ?>
-                </div>
-            </div>
-        </a>
+        </div>
     </div>
 <?php else: ?>
-    <?php $affiliateLink = affilicious_get_product_affiliate_link($product); ?>
-
     <div class="product-thumbnail">
-        <a href="<?php echo $affiliateLink; ?>" rel="nofollow" target="_blank">
-            <?php if(has_post_thumbnail()): ?>
+        <?php if(has_post_thumbnail()): ?>
+            <a href="<?php echo $affiliateLink; ?>" rel="nofollow" target="_blank">
                 <?php the_post_thumbnail(); ?>
-            <?php else: ?>
-                <i class="fa fa-question-circle-o fa-2x"></i>
-            <?php endif; ?>
-        </a>
+            </a>
+        <?php else: ?>
+            <i class="fa fa-question-circle-o fa-2x"></i>
+        <?php endif; ?>
     </div>
 <?php endif; ?>
