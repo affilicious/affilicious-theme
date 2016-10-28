9f5d162a25b3bb36b75c4ecf716ac162d713e53e
Alexander Barton <alexander.barton@outlook.com>
2016-10-28 18:49:37 +0200

    Added support for the default detail group

--- partials/content-product-details.php
+++ partials/content-product-details.php
@@ -2,28 +2,10 @@
     <tbody>
         <?php $product = aff_get_product(); ?>
         <?php $affiliateLink = aff_get_product_cheapest_affiliate_link($product); ?>
-        <?php $price = aff_get_product_cheapest_price($product); ?>
-        <?php if(!empty($price)): ?>
-            <tr>
-                <td><?php _e('Price', 'affilicious-theme'); ?></td>
-                <td>
-                    <?php if(!empty($affiliateLink)): ?>
-                    <a class="price" href="<?php echo $affiliateLink; ?>" itemprop="price" rel="nofollow" target="_blank">
-                        <?php echo $price; ?>
-                    </a>
-                    <?php else: ?>
-                    <span class="price" itemprop="price">
-                        <?php echo $price; ?>
-                    </span>
-                    <?php endif; ?>
-                </td>
-            </tr>
-        <?php endif; ?>
-
         <?php $details = aff_get_product_details(); ?>
         <?php if(!empty($details)): ?>
             <?php foreach ($details as $detail): ?>
-                <tr>
+                <tr data-detail-name="<?php echo $detail['name']; ?>">
                     <td><?php echo $detail['title']; ?></td>
                     <?php if($detail['type'] === 'file'): ?>
                         <td><?php echo wp_get_attachment_link($detail['value'], 'medium', false, false, __('Download', 'affiliate-theme')); ?></td>
