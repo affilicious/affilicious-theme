636d5004a4ff772cf66594b3459a635a83ae0ea4
Kristian Strehlau <krischi.s@web.de>
2016-12-06 22:35:15 +0100

    Added a new footer

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer id="footer" class="footer" role="contentinfo"
-        itemscope itemtype="http://schema.org/WPFooter">
+		itemscope itemtype="http://schema.org/WPFooter">
 
 	<?php if (function_exists('yoast_breadcrumb')): ?>
 		<div class="footer-breadcrumbs">
@@ -9,37 +9,66 @@
 		</div>
 	<?php endif; ?>
 
-	<div class="footer-content">
+	<div class="footer-top">
 		<div class="container">
-			<div class="clearfix">
-				<div class="footer-logo">
-					<a href="<?php home_url('/'); ?>"><?php bloginfo('name') ?></a>
-				</div>
-
-				<div id="bottom-menu">
-					<?php if (afft_has_bottom_1_menu()): ?>
-						<?php afft_bottom_1_menu(); ?>
-					<?php endif; ?>
-
-					<?php if (afft_has_bottom_2_menu()): ?>
-						<?php afft_bottom_2_menu(); ?>
-					<?php endif; ?>
-
-					<?php if (afft_has_bottom_3_menu()): ?>
-						<?php afft_bottom_3_menu(); ?>
-					<?php endif; ?>
-
-					<?php if (afft_has_bottom_4_menu()): ?>
-						<?php afft_bottom_4_menu(); ?>
-					<?php endif; ?>
+			<div class="row">
+				<div class="col-md-12">
+					<div class="footer-logo">
+						<a href="<?php home_url('/'); ?>"><?php bloginfo('name') ?></a>
+					</div>
 				</div>
 			</div>
-			<div class="footer-copyright text-center">
-				<?php echo get_theme_mod('afft-information-copyright-text'); ?>
+			<div class="row">
+				<div class="col-md-6 col-sm-12 col-xs-12">
+					<div class="footer-text">
+						<h4>Über uns</h4>
+						<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolo<p>
+					</div>
+				</div>
+				<div class="col-md-6 col-sm-12 col-xs-12">
+					<div class="footer-navigation">
+						<div style="display: inline-block; width: 45%;">
+							<?php if (afft_has_bottom_1_menu()): ?>
+								<?php afft_bottom_1_menu(); ?>
+							<?php endif; ?>
+						</div>
+						<div style="display: inline-block; width: 45%;">
+							<?php if (afft_has_bottom_2_menu()): ?>
+								<?php afft_bottom_2_menu(); ?>
+							<?php endif; ?>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="footer-bottom">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 col-sm-6 col-xs-12 flex-xs-middle">
+					<div class="footer-copyright">
+						<?php echo get_theme_mod('afft-information-copyright-text'); ?>
+						<ul>
+							<li><a href="#">Impressum</a></li>
+							<li><a href="#">AGB</a></li>
+							<li><a href="#">Kontakt</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-md-6 col-sm-6 col-xs-12 flex-xs-middle">
+					<div class="footer-social">
+						<a href="#" class="social-button facebook"><i class="fa fa-facebook"></i></a>
+						<a href="#" class="social-button twitter"><i class="fa fa-twitter"></i></a>
+						<a href="#" class="social-button google"><i class="fa fa-google-plus"></i></a>
+						<a href="#" class="social-button pinterest"><i class="fa fa-pinterest"></i></a>
+						<a href="#" class="social-button reddit"><i class="fa fa-reddit"></i></a>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
 </footer>
+
 
 <?php wp_footer(); ?>
 </body>
