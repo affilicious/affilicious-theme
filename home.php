35dbf1c73f3c0829fd060c5b932756a949a9cd8d
Alexander Barton <alexander.barton@outlook.com>
2016-12-20 21:36:53 +0100

    Optimized the sidebar cols width

--- home.php
+++ home.php
@@ -3,7 +3,7 @@
 <main id="content" role="main" itemprop="mainContentOfPage" itemscope itemtype="http://schema.org/Blog">
 	<div class="container">
 		<div class="row">
-			<div class="col-md-8 col-xs-12">
+			<div class="col-xl-8 col-lg-8 col-md-8 col-sm-12 col-xs-12">
 				<div class="row">
 					<?php if (have_posts()): ?>
 						<?php $count = 0; ?>
@@ -26,7 +26,7 @@
 					<?php endif; ?>
 				</div>
 			</div>
-			<div class="col-md-4 col-xs-12">
+			<div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12">
 				<?php get_sidebar(); ?>
 			</div>
 		</div>
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <main id="content" role="main" itemprop="mainContentOfPage" itemscope itemtype="http://schema.org/Blog">
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-xs-12">
+            <div class="col-xl-8 col-lg-8 col-md-8 col-sm-12 col-xs-12">
                 <?php if (have_posts()): ?>
                     <?php while (have_posts()) : the_post(); ?>
                         <?php get_template_part('partials/content-entry'); ?>
@@ -12,7 +12,7 @@
                     <?php get_template_part('partials/content-none'); ?>
                 <?php endif; ?>
             </div>
-            <div class="col-md-4 col-xs-12">
+            <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12">
                 <?php get_sidebar(); ?>
             </div>
         </div>
