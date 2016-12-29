7b37e747059140c111c10f2cf39b5a28ae3b6eab
Alexander Barton <alexander.barton@outlook.com>
2016-12-29 17:46:51 +0100

    Fixed the add image offset error

--- partials/misc/no-thumbnail.php
+++ partials/misc/no-thumbnail.php
@@ -5,7 +5,7 @@
         <?php _e('No image available', 'affilicious-theme'); ?>
     </p>
 
-    <?php if(current_user_can('edit_post')): ?>
+    <?php if(is_user_logged_in() && current_user_can('edit_post')): ?>
         <a class="no-thumbnail-add-image" href="<?php echo admin_url('upload.php')?>">
             <?php _e('Add now', 'affilicious-theme'); ?>
         </a>
