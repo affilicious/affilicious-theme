59b9c87f78f735794d408dd78a3fd1065aecb135
Alexander Barton <alexander.barton@outlook.com>
2016-12-30 17:28:52 +0100

    Fixed the styling of the pages

--- partials/entry/teaser.php
+++ partials/entry/teaser.php
@@ -1,7 +1,11 @@
-<div class="entry-thumbnail <?php if(!has_post_thumbnail()): ?>entry-no-thumbnail<?php endif; ?>">
-    <?php get_template_part('partials/entry/thumbnail'); ?>
-</div>
+<?php if(!is_page() || has_post_thumbnail()): ?>
+    <div class="entry-thumbnail <?php if(!has_post_thumbnail()): ?>entry-no-thumbnail<?php endif; ?>">
+        <?php get_template_part('partials/entry/thumbnail'); ?>
+    </div>
+<?php endif; ?>
 
-<footer class="entry-bar">
-    <?php get_template_part('partials/entry/bar'); ?>
-</footer>
+<?php if(!is_page()): ?>
+    <footer class="entry-bar">
+        <?php get_template_part('partials/entry/bar'); ?>
+    </footer>
+<?php endif; ?>
