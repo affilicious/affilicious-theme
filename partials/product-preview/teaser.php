11d57191b9813b46e37812081dfff75e9ad56113
Alexander Barton <alexander.barton@outlook.com>
2016-12-30 23:16:13 +0100

    The product price tag is now dynamic

--- partials/product-preview/teaser.php
+++ partials/product-preview/teaser.php
@@ -1,20 +1,19 @@
 <div class="product-preview-thumbnail <?php if(!has_post_thumbnail()): ?>product-preview-no-thumbnail<?php endif; ?>">
     <?php get_template_part('partials/product-preview/thumbnail'); ?>
 
-    <div class="product-preview-tag-bar">
-        <?php
-            aff_the_product_tags(
-                null,
-                '<span class="product-preview-tag-item tag">',
-                '</span>'
-            );
-        ?>
+    <?php if(aff_has_product_price() || aff_has_product_tags()): ?>
+        <div class="product-preview-tag-bar">
+            <?php aff_the_product_tags(null, '<span class="product-preview-tag-item tag">', '</span>'); ?>
 
-        <span class="product-preview-tag-item tag tag-price">300.00 €</span>
-    </div>
-
+            <?php if(aff_has_product_price()): ?>
+                <span class="product-preview-tag-item tag tag-price"><?php aff_the_product_price(); ?></span>
+            <?php endif; ?>
+        </div>
+    <?php endif; ?>
 </div>
 
-<small class="product-preview-price-indication text-muted">
-    <?php aff_the_shop_price_indication(); ?>
-</small>
+<?php if(aff_has_product_price()): ?>
+    <small class="product-preview-price-indication text-muted">
+        <?php aff_the_shop_price_indication(); ?>
+    </small>
+<?php endif; ?>
